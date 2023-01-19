5520e854dd700b35b7bd47419aac01afe21b100a
msyavuz <salih.yavuz@metu.edu.tr>
2023-01-19 22:52:57 +0300

    mobile navbar fix

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -12,7 +12,7 @@ function ProjectCard(props: projectCardProps) {
     const [showDescription, setShowDescription] = useState(false);
     return (
         <div
-            className="project-card relative scale-90 overflow-x-hidden col-span-1 text-center text-lg hover:scale-110 transition-all rounded hover:z-10"
+            className="project-card relative scale-90 overflow-x-hidden col-span-1 text-center text-lg hover:scale-110 transition-all rounded"
             onMouseEnter={() => {
                 setShowDescription(true);
             }}
--- src/components/sections/Navbar.tsx
+++ src/components/sections/Navbar.tsx
@@ -5,7 +5,7 @@ function Navbar() {
     const [isOpen, setIsOpen] = useState(true);
 
     return (
-        <nav className="flex place-content-end items-center w-full h-12 lg:h-24 p-4 lg:p-8 font-bold text-sm lg:text-2xl text-center text-white sticky top-0 shadow bg-slate-800">
+        <nav className="flex place-content-end items-center w-full h-12 lg:h-24 p-4 lg:p-8 font-bold text-sm lg:text-2xl text-center text-white sticky top-0 shadow bg-slate-800 z-10">
             <ul className={`${isOpen ? "visible" : "hidden"} lg:p-2`}>
                 <div className="flex justify-end gap-1 lg:gap-12 p-2">
                     <li className="px-2 lg:px-4 py-2 hover:bg-slate-400 hover:text-black rounded">
