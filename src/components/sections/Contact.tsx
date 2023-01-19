5af426bd4c2d98ef4b1bb95e02431669fa3b409b
msyavuz <salih.yavuz@metu.edu.tr>
2023-01-19 23:33:59 +0300

    lighthouse recommendation

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -6,16 +6,25 @@ function Contact() {
             id="contact"
             className="text-white flex justify-center items-center gap-8 text-3xl mb-4 mt-16"
         >
-            <a href="https://github.com/msyavuz" target="_blank">
+            <a
+                href="https://github.com/msyavuz"
+                target="_blank"
+                aria-label="github page for msyavuz"
+            >
                 <BsGithub />
             </a>
             <a
                 href="https://www.linkedin.com/in/mehmetsalihyavuz/"
                 target="_blank"
+                aria-label="linkedin page for msyavuz"
             >
                 <BsLinkedin />
             </a>
-            <a href="https://www.instagram.com/msalihyavuz/" target="_blank">
+            <a
+                href="https://www.instagram.com/msalihyavuz/"
+                target="_blank"
+                aria-label="instagram page for msyavuz"
+            >
                 <BsInstagram />
             </a>
         </section>
--- src/components/sections/Navbar.tsx
+++ src/components/sections/Navbar.tsx
@@ -6,21 +6,23 @@ function Navbar() {
 
     return (
         <nav className="flex place-content-end items-center w-full h-12 lg:h-24 p-4 lg:p-8 font-bold text-sm lg:text-2xl text-center text-white sticky top-0 shadow bg-slate-800 z-10">
-            <ul className={`${isOpen ? "visible" : "hidden"} lg:p-2`}>
-                <div className="flex justify-end gap-1 lg:gap-12 p-2">
-                    <li className="px-2 lg:px-4 py-2 hover:bg-slate-400 hover:text-black rounded">
-                        <a href="#about">About</a>
-                    </li>
-                    <li className="px-2 lg:px-4 py-2 hover:bg-slate-400 hover:text-black rounded">
-                        <a href="#tech">Tech</a>
-                    </li>
-                    <li className="px-2 lg:px-4 py-2 hover:bg-slate-400 hover:text-black rounded">
-                        <a href="#projects">Projects</a>
-                    </li>
-                    <li className="px-2 lg:px-4 py-2 hover:bg-slate-400 hover:text-black rounded">
-                        <a href="#contact">Contact</a>
-                    </li>
-                </div>
+            <ul
+                className={`${
+                    isOpen ? "visible" : "hidden"
+                } lg:p-2 flex justify-end gap-1 lg:gap-12 p-2`}
+            >
+                <li className="px-2 lg:px-4 py-2 hover:bg-slate-400 hover:text-black rounded">
+                    <a href="#about">About</a>
+                </li>
+                <li className="px-2 lg:px-4 py-2 hover:bg-slate-400 hover:text-black rounded">
+                    <a href="#tech">Tech</a>
+                </li>
+                <li className="px-2 lg:px-4 py-2 hover:bg-slate-400 hover:text-black rounded">
+                    <a href="#projects">Projects</a>
+                </li>
+                <li className="px-2 lg:px-4 py-2 hover:bg-slate-400 hover:text-black rounded">
+                    <a href="#contact">Contact</a>
+                </li>
             </ul>
             <button
                 className=" lg:ml-8 hover:text-black rounded"
